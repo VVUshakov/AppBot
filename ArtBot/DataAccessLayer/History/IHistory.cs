d4d9f77359b74ecd6978108c3342aee2a1208be0
Владимир Ушаков <svayznoy@inbox.ru>
2024-01-28 17:23:04 +0700

    Создать интерфейс IHistory.cs

---
+++ ArtBot/DataAccessLayer/History/IHistory.cs
@@ -0,0 +1,10 @@
+﻿namespace ArtBot.DataAccessLayer.History
+{
+    internal interface IHistory
+    {
+        /// <summary>
+        /// Интерфейс IUpdate определяет свойство класса History.
+        /// Свойство History хранит в себе историю изменений объекта.        
+        public History<string> History { get; }
+    }
+}
