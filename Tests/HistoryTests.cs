e7b47518bf07b0b7ab692fdb8ce9a138262ab7fa
Владимир Ушаков <svayznoy@inbox.ru>
2024-01-28 18:40:31 +0700

    Покрыть тестами класс History

--- Tests/HistoryTests.cs
+++ Tests/HistoryTests.cs
@@ -5,28 +5,7 @@ namespace Tests
     [TestClass]
     public class HistoryTests
     {
-        public History History;
-
-        //public List<string> History { get; set; }
-
-        public HistoryTests()
-        {
-            History = new List<string>();
-        }
-
-        [TestMethod]
-        public void TestGetHistory()
-        {
-            // Арrange
-            History.Add("Элемент 1");
-            History.Add("Элемент 2");
-
-            // Act
-            var result = History;
-
-            // Assert
-            Assert.AreEqual(2, result.Count);
-        }
+        
     }
 
 }
