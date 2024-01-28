7520017094cdfff95da6ff7868d55130b732c39b
Владимир Ушаков <svayznoy@inbox.ru>
2024-01-28 17:44:23 +0700

    Добавить класс History

--- ArtBot/DataAccessLayer/History/History.cs
+++ ArtBot/DataAccessLayer/History/History.cs
@@ -1,64 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ArtBot.DataAccessLayer.History
+﻿namespace ArtBot.DataAccessLayer.History
 {
+    /// <summary>
+    /// Этот класс может быть использован для отслеживания изменений в объекте другого класса. 
+    /// Для этого вы должны создать экземпляр класса ChangeHistory в отслеживаемом классе
+    /// и вызвать метод Add каждый раз, когда значение свойства объекта изменяется.
+    /// /// </summary>
     public class History
     {
-        /// <summary>
-        /// Этот класс может быть использован для отслеживания изменений в объекте другого класса. 
-        /// Для этого вы должны создать экземпляр класса ChangeHistory в отслеживаемом классе
-        /// и вызвать метод Add каждый раз, когда значение свойства объекта изменяется.
-        /// /// </summary>
-
         private readonly Dictionary<string, Dictionary<DateTime, string>> _history = [];
 
-        public void Add(string propertyName, string newValue, DateTime time)
-        {
-            /// <summary>
-            /// Метод Add используется для добавления новой пары ключ-значение в историю изменений объекта.
-            /// Он добавляет новую пару ключ-значение по запрашиваемому свойству и времени изменения.
-            /// Если свойство не найдено, будет создан новый словарь.
-            /// </summary>
-            /// <param name="propertyName">Имя изменяемого свойства</param>
-            /// <param name="newValue">Новое значение свойства</param>
-            /// <param name="time">Время изменения</param>
-            _history[propertyName].Add(time, newValue);
-        }
+        /// <summary>
+        /// Метод Add используется для добавления новой пары ключ-значение в историю изменений объекта.
+        /// Он добавляет новую пару ключ-значение по запрашиваемому свойству и времени изменения.
+        /// Если свойство не найдено, будет создан новый словарь.
+        /// </summary>
+        /// <param name="propertyName">Имя изменяемого свойства</param>
+        /// <param name="newValue">Новое значение свойства</param>
+        /// <param name="time">Время изменения</param>
+        public void Add(string propertyName, string newValue, DateTime time) => _history[propertyName].Add(time, newValue);
 
-        public Dictionary<string, Dictionary<DateTime, string>> GetAllHistory()
-        {
-            /// <summary>
-            /// Метод GetAllHistory возвращает всю историю изменений объекта класса.
-            /// </summary>
-            /// <returns>История изменений объекта</returns>
+        /// <summary>
+        /// Метод GetAllHistory возвращает всю историю изменений объекта класса.
+        /// </summary>
+        /// <returns>История изменений объекта</returns>
+        public Dictionary<string, Dictionary<DateTime, string>> GetAllHistory() => _history;
 
-            return _history;
-        }
+        /// <summary>
+        /// Метод GetHistory возвращает список всех изменений для указанного свойства.
+        /// </summary>
+        /// <param name="propertyName">Имя изменяемого свойства</param>
+        /// <returns>История изменений для указанного свойства</returns>
+        public Dictionary<DateTime, string> GetHistory(string propertyName) => _history[propertyName];
 
-        public Dictionary<DateTime, string> GetHistory(string propertyName)
-        {
-            /// <summary>
-            /// Метод GetHistory возвращает список всех изменений для указанного свойства.
-            /// </summary>
-            /// <param name="propertyName">Имя изменяемого свойства</param>
-            /// <returns>История изменений для указанного свойства</returns>
-
-            return _history[propertyName];
-        }
-
+        /// <summary>
+        /// Метод GetValueOnDate возвращает актуальное значение запрашиваемого свойства на запрашиваемую дату или на ближайшую более раннюю найденную дату.
+        /// </summary>
+        /// <param name="propertyName">Имя изменяемого свойства</param>
+        /// <param name="date">Запрашиваемая дата</param>
+        /// <returns>Значение свойства на запрашиваемую дату</returns>
         public string GetValueOnDate(string propertyName, DateTime date)
         {
-            /// <summary>
-            /// Метод GetValueOnDate возвращает актуальное значение запрашиваемого свойства на запрашиваемую дату или на ближайшую более раннюю найденную дату.
-            /// </summary>
-            /// <param name="propertyName">Имя изменяемого свойства</param>
-            /// <param name="date">Запрашиваемая дата</param>
-            /// <returns>Значение свойства на запрашиваемую дату</returns>
-
             if (!_history.TryGetValue(propertyName, out Dictionary<DateTime, string>? value))
             {
                 // Проверяем, есть ли в истории изменений запрашиваемое свойство.
@@ -84,13 +65,5 @@ namespace ArtBot.DataAccessLayer.History
                 return valueOnDate.Value;
             }
         }
-    }
-}
-
-namespace ArtBot.DataAccessLayer.History
-{
-    public class History<T>
-    {
-        
     }
 }
--- ArtBot/DataAccessLayer/History/IHistory.cs
+++ ArtBot/DataAccessLayer/History/IHistory.cs
@@ -1,5 +1,13 @@
 ﻿namespace ArtBot.DataAccessLayer.History
 {
+    /// <summary>
+    /// Интерфейс IHistory определяет свойство класса History.
+    /// Свойство History хранит в себе историю изменений объекта.
+    public interface IHistory
+    {
+        private History<string> _history;
+    }
+
     internal interface IHistory
     {
         /// <summary>
