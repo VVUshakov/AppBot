2fa9ac4ad111efdfd5387dc30c2b09491b9fa560
Владимир Ушаков <svayznoy@inbox.ru>
2024-01-31 22:24:52 +0700

    Вынести дублирующийся код в метод SetUp в классе HistoryTest

--- Tests/NUnitTests/DataAccessLayer/History/HistoryTest.cs
+++ Tests/NUnitTests/DataAccessLayer/History/HistoryTest.cs
@@ -1,48 +1,54 @@
 using ArtBot.DataAccessLayer.History;
+using Microsoft.VisualBasic;
+using NUnit.Framework.Internal;
 
 
 namespace NUnitTests
 {
     [TestFixture]
     public class HistoryTest
-    {        
+    {
+        private History history;
+
+        string propertyName1;
+        string propertyName2;
+        DateTime dateAndTime1;
+        DateTime dateAndTime2;
+        string newValue1;
+        string newValue2;
+
+        [SetUp]
+        public void SetUp()
+        {
+            // Arrange
+            history = new History();
+            propertyName1 = "PropertyName1";
+            propertyName2 = "PropertyName2";
+            dateAndTime1 = DateTime.Now;
+            dateAndTime2 = DateTime.Now.AddDays(1);
+            newValue1 = "NewValue1";
+            newValue2 = "NewValue2";
+
+            // Act
+            history.Add(propertyName1, dateAndTime1, newValue1);
+            history.Add(propertyName2, dateAndTime2, newValue2);
+        }
+
         [Test]
         public void TestAdd()
         {
-            // Arrange
-            var history = new History();
-            var propertyName = "PropertyName";
-            var dateAndTime = DateTime.Now;
-            var newValue = "NewValue";
-
-            // Act
-            history.Add(propertyName, dateAndTime, newValue);
-
             // Assert
             Assert.Multiple(() =>
             {
-                Assert.That(history.HistoryChanges.ContainsKey(propertyName), Is.True);
-                Assert.That(history.HistoryChanges[propertyName].ContainsKey(dateAndTime), Is.True);
-                Assert.That(history.HistoryChanges[propertyName][dateAndTime], Is.EqualTo(newValue));
+                Assert.That(history.HistoryChanges.ContainsKey(propertyName1), Is.True);
+                Assert.That(history.HistoryChanges[propertyName1].ContainsKey(dateAndTime1), Is.True);
+                Assert.That(history.HistoryChanges[propertyName1][dateAndTime1], Is.EqualTo(newValue1));
             });
         }
 
         [Test]
         public void TestGetAllHistory()
         {
-            // Arrange
-            var history = new History();
-            var propertyName1 = "PropertyName1";
-            var propertyName2 = "PropertyName2";
-            var dateAndTime1 = DateTime.Now;
-            var dateAndTime2 = DateTime.Now.AddDays(1);
-            var newValue1 = "NewValue1";
-            var newValue2 = "NewValue2";
-
-            // Act
-            history.Add(propertyName1, dateAndTime1, newValue1);
-            history.Add(propertyName2, dateAndTime2, newValue2);
-
             // Assert
             var expectedHistory = new Dictionary<string, Dictionary<DateTime, string>>
             {
@@ -55,19 +61,6 @@ namespace NUnitTests
         [Test]
         public void TestGetHistoryProperty()
         {
-            // Arrange
-            var history = new History();
-            var propertyName1 = "PropertyName1";
-            var propertyName2 = "PropertyName2";
-            var dateAndTime1 = DateTime.Now;
-            var dateAndTime2 = DateTime.Now.AddDays(1);
-            var newValue1 = "NewValue1";
-            var newValue2 = "NewValue2";
-
-            // Act
-            history.Add(propertyName1, dateAndTime1, newValue1);
-            history.Add(propertyName2, dateAndTime2, newValue2);
-
             // Assert
             var expectedHistory1 = new Dictionary<DateTime, string> { { dateAndTime1, newValue1 } };
             var expectedHistory2 = new Dictionary<DateTime, string> { { dateAndTime2, newValue2 } };
@@ -81,25 +74,13 @@ namespace NUnitTests
         [Test]
         public void TestGetValuePropertyOnDate()
         {
-            // Arrange
-            var history = new History();
-            var propertyName = "PropertyName";
-            var dateAndTime1 = DateTime.Now;
-            var dateAndTime2 = DateTime.Now.AddDays(1);
-            var newValue1 = "NewValue1";
-            var newValue2 = "NewValue2";
-
-            // Act
-            history.Add(propertyName, dateAndTime1, newValue1);
-            history.Add(propertyName, dateAndTime2, newValue2);
-
             // Assert
             var expectedValue1 = newValue1;
             var expectedValue2 = newValue2;
             Assert.Multiple(() =>
             {
-                Assert.That(history.GetValuePropertyOnDate(propertyName, dateAndTime1), Is.EqualTo(expectedValue1));
-                Assert.That(history.GetValuePropertyOnDate(propertyName, dateAndTime2), Is.EqualTo(expectedValue2));
+                Assert.That(history.GetValuePropertyOnDate(propertyName1, dateAndTime1), Is.EqualTo(expectedValue1));
+                Assert.That(history.GetValuePropertyOnDate(propertyName2, dateAndTime2), Is.EqualTo(expectedValue2));
             });
         }
     }
