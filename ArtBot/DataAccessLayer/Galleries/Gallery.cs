3d17fc0b666fc460bce064b7e75dead7a603a4ce
Владимир Ушаков <svayznoy@inbox.ru>
2024-02-03 09:29:10 +0700

    Добавить определения методов используя using static System.Net.Mime.MediaTypeNames;

--- ArtBot/DataAccessLayer/Galleries/Gallery.cs
+++ ArtBot/DataAccessLayer/Galleries/Gallery.cs
@@ -1,12 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using static System.Net.Mime.MediaTypeNames;
 
 namespace ArtBot.DataAccessLayer.Galleries
 {
-    internal class Gallery
+    public class Gallery
     {
+        private List<Image> _images = new List<Image>();
+
+
+        public void Add(Image image)
+        {
+            _images.Add(image);
+        }
+
+        public void Remove(Image image)
+        {
+            _images.Remove(image);
+        }
+
+        public void Update(Image image)
+        {
+            _images.Remove(image);
+            _images.Add(image);
+        }
+
+        public void Clear()
+        {
+            _images.Clear();
+        }
+
+        public List<Image> GetGallery()
+        {
+            return _images;
+        }
     }
 }
