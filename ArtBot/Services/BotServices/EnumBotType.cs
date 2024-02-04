b0a220c20342b9b9a13b7dd7f2dd29347e427c1e
Владимир Ушаков <svayznoy@inbox.ru>
2024-02-04 13:45:15 +0700

    Добавить перечислитель ботов enum BotType

---
+++ ArtBot/Services/BotServices/EnumBotType.cs
@@ -0,0 +1,15 @@
+﻿namespace ArtBot.Services.BotServices
+{
+    /// <summary>
+    /// BotType - это перечисление, которое содержит возможные типы ботов. 
+    /// Вы можете добавить другие типы ботов, если это необходимо.
+    /// </summary>
+    public enum BotType
+    {
+        Telegram,
+        VK,
+        Viber,
+        WhatsApp,
+    }
+
+}
