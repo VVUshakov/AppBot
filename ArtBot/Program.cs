841285f5b2d22e3ab80be4b2f85532790990eeb5
Владимир Ушаков <svayznoy@inbox.ru>
2024-02-11 20:01:55 +0700

    Удалить комментарии в точке входа

--- ArtBot/Program.cs
+++ ArtBot/Program.cs
@@ -3,13 +3,6 @@ using ArtBot.Services.Logging;
 using ArtBot.Services.RequestHandlerService;
 
 
-/// <summary>
-/// В этом примере класс Program реализует точку входа приложения, в которой создаются экземпляры всех сервисов и запускаются их методы StartAsync. 
-/// Затем приложение получает входящий запрос от клиента с помощью метода GetRequestFromClientAsync и передает его в сервис RequestHandlerService для обработки.
-/// Метод GetRequestFromClientAsync представляет собой точку взаимодействия с клиентом и может быть реализован с использованием соответствующих средств коммуникации, 
-/// таких как HTTP-сервер, TCP-сокеты или другие методы обмена данными.
-/// </summary>
-/// <param name="args"></param>
 class Program
 {
     static async Task Main(string[] args)
