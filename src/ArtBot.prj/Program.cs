d25e88c2d66aa65d17dadcb755a3cf340c6e81a5
Владимир Ушаков <svayznoy@inbox.ru>
2024-02-18 13:02:36 +0700

    Закончить изменения структуры проекта

--- src/ArtBot.prj/Program.cs
+++ src/ArtBot.prj/Program.cs
@@ -2,46 +2,47 @@
 using ArtBot.Services.Logging;
 using ArtBot.Services.RequestHandlerService;
 
+namespace artBot;
 
-class Program
+internal class Program
 {
-    static async Task Main(string[] args)
-    {
-        var telegramBot = new TelegramBot();
-        var requestHandlerService = new RequestHandlerService();
+	private static async Task Main(string[] args)
+	{
+		var telegramBot = new TelegramBot();
+		var requestHandlerService = new RequestHandlerService();
 
 
 
-        await telegramBot.StartAsync();
-        await requestHandlerService.StartAsync();
-        await LoggingService.StartAsync();
+		await telegramBot.StartAsync();
+		await requestHandlerService.StartAsync();
+		await LoggingService.StartAsync();
 
-        /*
-        // Сконфигурировать параметры загрузки приложения
-        var configuration = new Configuration();
+		/*
+	// Сконфигурировать параметры загрузки приложения
+	var configuration = new Configuration();
 
-        // Запустить все сервисы и боты
-        IService services = new Service(configuration.ListServices);
-        await services.StartAsync();
-        */
-
-
+	// Запустить все сервисы и боты
+	IService services = new Service(configuration.ListServices);
+	await services.StartAsync();
+	*/
 
 
 
-        // Получение входящего запроса от клиента
-        string request = await GetRequestFromClientAsync();
 
 
-        // Обработка запроса
-        //await requestHandlerService.HandleRequestAsync(request);
+		// Получение входящего запроса от клиента
+		var request = await GetRequestFromClientAsync();
 
-    }
 
-    static async Task<string> GetRequestFromClientAsync()
-    {
-        // Взаимодействие с клиентом для получения входящего запроса
-        // ...
-        return "REQUEST";
-    }
+		// Обработка запроса
+		//await requestHandlerService.HandleRequestAsync(request);
+
+	}
+
+	private static Task<string> GetRequestFromClientAsync()
+	{
+		// Взаимодействие с клиентом для получения входящего запроса
+		// ...
+		return Task.FromResult("REQUEST");
+	}
 }
